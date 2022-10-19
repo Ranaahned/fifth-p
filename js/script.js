23f12e1bedd07d59ca79f052273c3ace38588173
Ranaahned <raaan6114@gmail.com>
2022-10-19 14:05:04 +0200

    five

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+ var butr=document.getElementById("read-m")
+ 
+
+butr.onclick= function(){
+    var butr=document.getElementById("read-m")
+    butr.style.height="40px"
+    butr.style.margin="auto"
+
+    var text =document.getElementById("demos")
+     console.log("rana")
+    text.style.color="rgb(173, 126, 56)"
+    text.style.marginLeft="40px"
+    text.style.marginTop="20px"
+    
+    }
+
+
+
+
+
+    
+
